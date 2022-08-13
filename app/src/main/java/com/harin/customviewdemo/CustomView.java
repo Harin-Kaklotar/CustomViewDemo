926f22fde57a334690c5a6c945f0481db4bd4b96
Harin Kaklotar <harinlive@gmail.com>
2022-08-13 16:13:29 +0530

    Add attributes
    Add color and size attribute for CustomView

--- app/src/main/java/com/harin/customviewdemo/CustomView.java
+++ app/src/main/java/com/harin/customviewdemo/CustomView.java
@@ -1,6 +1,7 @@
 package com.harin.customviewdemo;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,9 +13,11 @@ import androidx.annotation.Nullable;
 
 public class CustomView extends View {
 
-    Rect mRect;
-    Paint mPaint;
-    private static final int DEFAULT_SQUARE_SIZE = 100;
+    private Rect mRect;
+    private Paint mSquarePaint;
+    private static final int DEFAULT_SQUARE_SIZE = 200;
+    private int squareColor;
+    private int squareSize;
 
     public CustomView(Context context) {
         super(context);
@@ -36,32 +39,41 @@ public class CustomView extends View {
         init(attrs);
     }
 
-    private void init(AttributeSet attrs) {
+    private void init(@Nullable AttributeSet attrs) {
         mRect = new Rect();
+        mSquarePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-        mRect.top = 10;
-        mRect.left = 10;
-        mRect.bottom = mRect.top + DEFAULT_SQUARE_SIZE;
-        mRect.right = mRect.left + DEFAULT_SQUARE_SIZE;
+        if (attrs == null)
+            return;
 
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setColor(Color.RED);
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomView);
+
+        squareColor = typedArray.getColor(R.styleable.CustomView_square_color, Color.GREEN);
+        squareSize = typedArray.getDimensionPixelSize(R.styleable.CustomView_square_size, DEFAULT_SQUARE_SIZE);
+
+        mSquarePaint.setColor(squareColor);
+
+        typedArray.recycle();
     }
 
     /**
      * change color
      */
     public void changeColor() {
-        mPaint.setColor(
-                mPaint.getColor() == Color.RED ? Color.GREEN : Color.RED
+        mSquarePaint.setColor(
+                mSquarePaint.getColor() == squareColor ? Color.GREEN : squareColor
         );
         postInvalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
 
-        canvas.drawRect(mRect, mPaint);
+        mRect.top = 10;
+        mRect.left = 10;
+        mRect.bottom = mRect.top + squareSize;
+        mRect.right = mRect.left + squareSize;
+
+        canvas.drawRect(mRect, mSquarePaint);
     }
 }
